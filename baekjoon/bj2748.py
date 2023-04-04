b429f4159c00b1626f7377b10d5fc90cce8a6ec1
YuJin Kwon <tokyj515@naver.com>
2023-04-04 22:36:02 +0900

    23.04.04

---
+++ baekjoon/bj2748.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def fibo(N):
+    if N == 0 or N == 1:
+        return dp[N]
+
+    if dp[N] == 0:
+        dp[N] = fibo(N - 2) + fibo(N - 1)
+
+    return dp[N];
+
+
+N = int(sys.stdin.readline())
+
+dp = [0] * 95
+dp[1] = 1
+
+print(fibo(N))
